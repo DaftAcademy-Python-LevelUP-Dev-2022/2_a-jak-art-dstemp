1c2781e899c8657b557f651a2f4c01e3a073aa88
Damian Stempel <d.stempel@kogena.com>
2022-05-22 20:33:46 +0200

    sum_of_str_elements

--- file.py
+++ file.py
@@ -7,7 +7,21 @@ def greeter(func):
 
 
 def sums_of_str_elements_are_equal(func):
-    pass
+    def inner(*args):
+        string = func(*args)
+        numbers = string.split(' ')
+        result = list()
+        for number in numbers:
+            if number[0] == '-':
+                result.append(-sum([int(digit) for digit in number[1:]]))
+            else:
+                result.append(sum([int(digit) for digit in number]))
+        if result[0] == result[1]:
+            sign = '=='
+        else:
+            sign = '!='
+        return f"{result[0]} {sign} {result[1]}"
+    return inner
 
 
 def format_output(*required_keys):
